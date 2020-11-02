8ccd572d6e66a1789b08f7c9d30f49480804e67c
twtrubiks <twtrubiks@gmail.com>
2020-11-02 10:58:21 +0800

    migrate v14 - demo_odoo_tutorial_wizard

--- demo_odoo_tutorial_wizard/models/models.py
+++ demo_odoo_tutorial_wizard/models/models.py
@@ -8,14 +8,12 @@ class DemoOdooWizardTutorial(models.Model):
     name = fields.Char('Description', required=True)
     partner_id = fields.Many2one('res.partner', string='Partner')
 
-    @api.multi
     def action_context_demo(self):
         # if self._context.get('context_data', False):
         if self.env.context.get('context_data'):
             raise ValidationError('have context data')
         raise ValidationError('hello')
 
-    @api.multi
     def action_button(self):
         for record in self:
             record.with_context(context_data=True).action_context_demo()
